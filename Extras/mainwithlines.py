b468c8d49d7e1ada38e55d7be0a3840c4120d8c6
unknown <mansari@g.clemson.edu>
2024-04-27 00:54:00 -0500

    2nd commit

---
+++ Extras/mainwithlines.py
@@ -0,0 +1,170 @@
+import streamlit as st
+import folium
+from streamlit_folium import st_folium
+
+# Set page config
+st.set_page_config(layout="wide")
+
+# Set page title
+APP_TITLE = """
+Imām al-Bukhārī's Riḥla
+"""
+SUB_HEADER = """
+Researched by Muntasir Zaman. 
+Visit my blog at: https://hadithnotes.org/
+"""
+st.title(APP_TITLE)
+st.subheader(SUB_HEADER)
+
+# LOCATIONS
+KABA = [21.42255297077458, 39.82618611908724]
+BUKHARA = [39.7681, 64.4556]
+BAGHDAD = [33.2622, 44.2470]
+
+# CUSTOM ICONS
+kaba_image = 'Icons/kaaba.png'
+kaba_icon = folium.CustomIcon(
+    kaba_image,
+    icon_size=(20,20)
+)
+
+red_star = 'Icons/red-star.png'
+red_star_icon = folium.CustomIcon(
+    red_star,
+    icon_size=(20,20)
+)
+
+black_dot = 'Icons/black-dot.png'
+black_dot_icon = folium.CustomIcon(
+    black_dot,
+    icon_size=(40,40)
+)
+
+# Intialize Map
+m = folium.Map(location=[29.31, 47.41], zoom_start=5, tiles='Stadia.StamenWatercolor', attr='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors')
+
+
+# Add locations and text to the map
+
+# 1. Makkah
+folium.Marker(
+    location = KABA, 
+    icon = kaba_icon,
+    popup = 'Makkah'
+).add_to(m)
+
+div_html ="""
+<div style="font-family: Brill, sans-serif; font-size: 8pt; text-align: left; color: black">
+    Makkah <br>
+    First time visited in 210 AH.
+</div>
+"""
+folium.map.Marker(
+    [KABA[0]-0.5, KABA[1]-0.1],
+    icon=folium.DivIcon(
+        icon_size=(250,36),
+        icon_anchor=(0,0),
+        html=div_html,
+        )
+    ).add_to(m)
+
+#2. Bukahara
+folium.Marker(
+    location = BUKHARA , 
+    icon = red_star_icon,
+    popup = 'Bukhara'
+).add_to(m)
+
+div_html ="""
+<div style="font-family: Brill, sans-serif; font-size: 8pt; text-align: left; color: black">
+    Bukhara <br>
+    Born here in 194 AH.
+</div>
+"""
+folium.map.Marker(
+    [BUKHARA[0]-0.3, BUKHARA[1]-0.1],
+        icon=folium.DivIcon(
+        icon_size=(250,36),
+        icon_anchor=(0,0),
+        html=div_html,
+        )
+    ).add_to(m)
+
+#3. Baghdad
+folium.Marker(
+    location = BAGHDAD, 
+    icon = black_dot_icon,
+    popup = 'Baghdad'
+).add_to(m)
+
+div_html ="""
+<div style="font-family: Brill, sans-serif; font-size: 8pt; text-align: left; color: black">
+    Baghdad
+</div>
+"""
+folium.map.Marker(
+    [BAGHDAD[0]+0.8, BAGHDAD[1]-0.5],
+        icon=folium.DivIcon(
+        icon_size=(250,36),
+        icon_anchor=(0,0),
+        html=div_html,
+        )
+    ).add_to(m)
+
+# Add Polylines
+BUKHARA_to_BAGHDAD=[
+    BUKHARA,
+    [38.365943567086376, 63.34372099695885],
+    [35.68524911544845, 58.418099097917136],
+    [34.111382700828514, 52.724847812011774],
+    [33.36665999541276, 46.87167373717649],
+    BAGHDAD
+]
+
+BAGHDAD_to_MAKKAH=[
+    BAGHDAD,
+    [30.729891948661574, 44.51756526465571],
+    [28.272300302456024, 43.87206833165582],
+    [25.92612917935021, 43.19967569311427],
+    [23.999993378286593, 42.12384747144779],
+    [21.994725443763087, 40.429418022323084],
+    KABA
+]
+
+# Bukhara to Baghdad
+folium.PolyLine(
+    locations=BUKHARA_to_BAGHDAD,
+    color='#000000',
+    weight=2,
+    smooth_factor=3,
+    tooltip="Bukhara to Baghdad"
+).add_to(m)
+folium.RegularPolygonMarker(location=BUKHARA_to_BAGHDAD[3],
+                            fill_color='black',
+                            color='black', #border color
+                            opacity = 0, #border opacity
+                            fillOpacity=1, 
+                            number_of_sides=3, 
+                            radius=9, 
+                            rotation=160).add_to(m)
+
+#Baghdad To Makkah
+folium.PolyLine(
+    locations=BAGHDAD_to_MAKKAH,
+    color='#000000',
+    weight=2,
+    smooth_factor=1,
+    tooltip="Baghdad to Makkah"
+).add_to(m)
+folium.RegularPolygonMarker(location=BAGHDAD_to_MAKKAH[3],
+                            fill_color='black',
+                            color='black', #border color
+                            opacity = 0, #border opacity
+                            fillOpacity=1, 
+                            number_of_sides=3, 
+                            radius=9, 
+                            rotation=100).add_to(m)
+
+st_data = st_folium(m, width=1400)
+
+
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import pandas as pd
 import folium
+import copy
 from streamlit_folium import st_folium
+import branca
+from math import comb as r
+from math import degrees 
+from math import atan
 
 # Set page config
 st.set_page_config(layout="wide")
@@ -16,155 +22,154 @@ Visit my blog at: https://hadithnotes.org/
 st.title(APP_TITLE)
 st.subheader(SUB_HEADER)
 
+show_popup = False
 # LOCATIONS
-KABA = [21.42255297077458, 39.82618611908724]
-BUKHARA = [39.7681, 64.4556]
-BAGHDAD = [33.2622, 44.2470]
+locations = pd.read_csv('Data/locations.csv')
+locations.set_index('location', inplace = True)
 
-# CUSTOM ICONS
-kaba_image = 'Icons/kaaba.png'
-kaba_icon = folium.CustomIcon(
-    kaba_image,
-    icon_size=(20,20)
-)
 
-red_star = 'Icons/red-star.png'
-red_star_icon = folium.CustomIcon(
-    red_star,
-    icon_size=(20,20)
-)
+#Brazier Curve function
+def bz(c,n=10,t=False):
+    m,q,p,s=list(zip(*c)), len(c),[],(n-1 if t else n)/1.
+    for i in range(n):
+        b=[r(q-1,v)*(i/s)**v*(1-(i/s))**(q-1-v)for v in range(q)]
+        p+=[(list(sum(j*k for j,k in zip(d,b))for d in m))]
+    return p
 
-black_dot = 'Icons/black-dot.png'
-black_dot_icon = folium.CustomIcon(
-    black_dot,
-    icon_size=(40,40)
-)
 
 # Intialize Map
-m = folium.Map(location=[29.31, 47.41], zoom_start=5, tiles='Stadia.StamenWatercolor', attr='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors')
+#m = folium.Map(location=[29.31, 47.41], zoom_start=5, tiles='Stadia.StamenWatercolor', attr='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Madina logo by Quda designs')
+m = folium.Map(location=[29.31, 47.41], zoom_start=5, tiles='Stadia.StamenWatercolor', attr='&')
 
+
+# Decalre feature groups
+feature_group_loc_names = folium.FeatureGroup(name='Display Names')
+feature_group_loc_icons = folium.FeatureGroup(name='Display Icons')
+fg_path_childhood = folium.FeatureGroup(name='Early Childhood')
 
 # Add locations and text to the map
+for index, loc in locations.iterrows():
+    #Icon
+    icon_path = loc['icon']
+    icon = folium.CustomIcon(icon_path, icon_size=(loc['icon_l'], loc['icon_w']))
 
-# 1. Makkah
-folium.Marker(
-    location = KABA, 
-    icon = kaba_icon,
-    popup = 'Makkah'
-).add_to(m)
+    #Popup
+    popup_html = str(loc['popup_html'])
+    iframe = branca.element.IFrame(html=popup_html , width=500, height=100)
+    popup = folium.Popup(iframe, max_width=500)
+    loc_icon = folium.Marker(
+                location = [loc['lat'], loc['long']], 
+                icon = icon,
+                popup = popup
+            )
+    loc_icon.add_to(feature_group_loc_icons)
 
-div_html ="""
-<div style="font-family: Brill, sans-serif; font-size: 8pt; text-align: left; color: black">
-    Makkah <br>
-    First time visited in 210 AH.
-</div>
+    # Add city names to the map
+
+    html = '<div style="font-size: 10pt; color:black">' + index + '</div>'
+    loc_name = folium.map.Marker(
+                        [loc['lat'] , loc['long']],
+                        icon=folium.DivIcon(
+                            icon_size=(250,36),
+                            icon_anchor=(-10,10),
+                            html=html,
+                            )
+                        )
+    
+    loc_name.add_to(feature_group_loc_names)
+    
+# Function to add path to the map
+def add_path(ft_group, startpoint, midpoint, endpoint, num_lines, line_color, line_wt, tooltip, arrow_color, arrow_size, arrow_dir_adjust=0):
+    # Add path
+    a = [locations.loc[startpoint]['lat'], locations.loc[startpoint]['long']]
+    b = [locations.loc[endpoint]['lat'], locations.loc[endpoint]['long']]
+    A_to_B = bz([a, *midpoint, b],
+            n=num_lines, t=True)
+    folium.PolyLine(
+        locations=A_to_B,
+        color=line_color,
+        weight=line_wt,
+        smooth_factor=0,
+        tooltip=tooltip
+    ).add_to(ft_group)
+
+    # Add arrow on the path
+    # Find slope
+    mid_of_line = int(len(A_to_B)/2)
+    slope = (A_to_B[mid_of_line][1]-A_to_B[mid_of_line-1][1]) / (A_to_B[mid_of_line][0]-A_to_B[mid_of_line-1][0])
+    angle = degrees(atan(slope))
+    folium.RegularPolygonMarker(location=A_to_B[mid_of_line],
+                                fill_color=arrow_color,
+                                color='black', #border color
+                                opacity = 0, #border opacity
+                                fillOpacity=1, 
+                                number_of_sides=3, 
+                                radius=arrow_size, 
+                                rotation=180-angle-arrow_dir_adjust).add_to(fg_path_childhood)
+
+# Bukahara to Merv  
+add_path(ft_group=fg_path_childhood, startpoint='Bukhara', midpoint=[[39.28, 62.72]], endpoint='Merv', num_lines = 100, line_wt=2, line_color = '#187e18',
+         tooltip = "Bukhara to Merv", arrow_size=6, arrow_color = '#000000'
+         )
+
+# Merv To Balkh
+add_path(ft_group=fg_path_childhood, startpoint='Merv', midpoint=[[36.71, 63.14],[38.00, 65.85]], endpoint='Balkh', num_lines = 150, line_wt=2, line_color = '#187e18',
+         tooltip = "Merv to Balkh", arrow_size=6, arrow_color = '#000000', arrow_dir_adjust=90
+         )
+
+# Balkh To Herat
+add_path(ft_group=fg_path_childhood, startpoint='Balkh', midpoint=[[34.63, 66.45]], endpoint='Herat', num_lines = 100, line_wt=2, line_color = '#187e18',
+         tooltip = "Balkh to Herat", arrow_size=6, arrow_color = '#000000', arrow_dir_adjust=90
+         )
+
+# Herat to Nishapur
+add_path(ft_group=fg_path_childhood, startpoint='Herat', midpoint=[[34.84, 59.18]], endpoint='Nishapur', num_lines = 100, line_wt=2, line_color = '#187e18',
+         tooltip = "Herat to Nishapur", arrow_size=6, arrow_color = '#000000', arrow_dir_adjust=-90
+         )
+
+
+
+
 """
-folium.map.Marker(
-    [KABA[0]-0.5, KABA[1]-0.1],
-    icon=folium.DivIcon(
-        icon_size=(250,36),
-        icon_anchor=(0,0),
-        html=div_html,
-        )
-    ).add_to(m)
-
-#2. Bukahara
-folium.Marker(
-    location = BUKHARA , 
-    icon = red_star_icon,
-    popup = 'Bukhara'
-).add_to(m)
-
-div_html ="""
-<div style="font-family: Brill, sans-serif; font-size: 8pt; text-align: left; color: black">
-    Bukhara <br>
-    Born here in 194 AH.
-</div>
-"""
-folium.map.Marker(
-    [BUKHARA[0]-0.3, BUKHARA[1]-0.1],
-        icon=folium.DivIcon(
-        icon_size=(250,36),
-        icon_anchor=(0,0),
-        html=div_html,
-        )
-    ).add_to(m)
-
-#3. Baghdad
-folium.Marker(
-    location = BAGHDAD, 
-    icon = black_dot_icon,
-    popup = 'Baghdad'
-).add_to(m)
-
-div_html ="""
-<div style="font-family: Brill, sans-serif; font-size: 8pt; text-align: left; color: black">
-    Baghdad
-</div>
-"""
-folium.map.Marker(
-    [BAGHDAD[0]+0.8, BAGHDAD[1]-0.5],
-        icon=folium.DivIcon(
-        icon_size=(250,36),
-        icon_anchor=(0,0),
-        html=div_html,
-        )
-    ).add_to(m)
-
-# Add Polylines
-BUKHARA_to_BAGHDAD=[
-    BUKHARA,
-    [38.365943567086376, 63.34372099695885],
-    [35.68524911544845, 58.418099097917136],
-    [34.111382700828514, 52.724847812011774],
-    [33.36665999541276, 46.87167373717649],
-    BAGHDAD
-]
-
-BAGHDAD_to_MAKKAH=[
-    BAGHDAD,
-    [30.729891948661574, 44.51756526465571],
-    [28.272300302456024, 43.87206833165582],
-    [25.92612917935021, 43.19967569311427],
-    [23.999993378286593, 42.12384747144779],
-    [21.994725443763087, 40.429418022323084],
-    KABA
-]
-
-# Bukhara to Baghdad
+# Bukhara to Merv
+A_to_B = bz([
+                [locations.loc['Bukhara']['lat'], locations.loc['Bukhara']['long']],
+                [39.52, 62.68],
+                [locations.loc['Merv']['lat'], locations.loc['Merv']['long']]
+            ],
+            n=100, t=True)
 folium.PolyLine(
-    locations=BUKHARA_to_BAGHDAD,
-    color='#000000',
-    weight=2,
-    smooth_factor=3,
-    tooltip="Bukhara to Baghdad"
-).add_to(m)
-folium.RegularPolygonMarker(location=BUKHARA_to_BAGHDAD[3],
+    locations=A_to_B,
+    color='#40514E',
+    weight=1,
+    smooth_factor=0,
+    tooltip="Bukhara to Merv"
+).add_to(fg_path_childhood)
+
+# Find slope
+mid_of_line = int(len(A_to_B)/2)
+slope = (A_to_B[mid_of_line][1]-A_to_B[mid_of_line-1][1]) / (A_to_B[mid_of_line][0]-A_to_B[mid_of_line-1][0])
+angle = degrees(atan(slope))
+folium.RegularPolygonMarker(location=A_to_B[mid_of_line],
                             fill_color='black',
                             color='black', #border color
                             opacity = 0, #border opacity
                             fillOpacity=1, 
                             number_of_sides=3, 
-                            radius=9, 
-                            rotation=160).add_to(m)
+                            radius=5, 
+                            rotation=180-angle).add_to(fg_path_childhood)
+    
+"""
 
-#Baghdad To Makkah
-folium.PolyLine(
-    locations=BAGHDAD_to_MAKKAH,
-    color='#000000',
-    weight=2,
-    smooth_factor=1,
-    tooltip="Baghdad to Makkah"
-).add_to(m)
-folium.RegularPolygonMarker(location=BAGHDAD_to_MAKKAH[3],
-                            fill_color='black',
-                            color='black', #border color
-                            opacity = 0, #border opacity
-                            fillOpacity=1, 
-                            number_of_sides=3, 
-                            radius=9, 
-                            rotation=100).add_to(m)
+ 
+feature_group_loc_names.add_to(m)
+feature_group_loc_icons.add_to(m)
+fg_path_childhood.add_to(m)
+folium.LayerControl().add_to(m)
 
-st_data = st_folium(m, width=1400)
+
+st.write(locations)
+st.write(locations.loc['Bukhara'])
+st_data = st_folium(m, width=1600) 
 
 
